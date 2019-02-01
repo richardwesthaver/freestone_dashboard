9199917e1dbe14e1e886e0832281f8cb48ff818b
Richard Westhaver <richard.westhaver@gmail.com>
2019-02-01 15:42:54 -0500

    some formatting

--- test.py
+++ test.py
@@ -8,6 +8,9 @@ import dash_bootstrap_components as dbc
 import plotly.graph_objs as go
 from datetime import datetime as dt
 
+# start up app
+app = dash.Dash('test', external_stylesheets=[dbc.themes.SANDSTONE])
+
 # collect data
 r = requests.get('https://docs.google.com/spreadsheets/d/1pZp7xbIEVOrM8Jk6xVwovIBe2I4sdv8u2h9RTwmiMZE/export?format=xlsx')
 raw_data = pd.ExcelFile(BytesIO(r.content))
@@ -15,6 +18,28 @@ data_set = pd.read_excel(raw_data, sheet_name=None)
 
 # constants
 today = dt.today()
+# gen_graph(data_set['Enrollments'],'enrollments','enrollments','user_id')
+# app layout
+app.layout = html.Div([
+    html.H1('Freestone Dashboard'),
+    html.Div([
+        html.H2('Summary')
+    ]),
+    html.Div([
+        html.H2('Key Metrics'),
+        dcc.Graph(id='Enrollments'),
+        dcc.Graph(id='Launches'),
+        dcc.Graph(id='Completions')
+    ]),
+    html.Div([
+        html.H2('Site Usage'),
+        dcc.Graph(id='Logins'),
+        html.Table(id='site_activity'),
+        dcc.Graph(id='device_usage')
+
+    ])
+]
+)
 
 #generate components
 def gen_graph(dataframe,id,title,y):
@@ -30,18 +55,13 @@ def gen_graph(dataframe,id,title,y):
         )
     )
 
-def gen_datePicker(id,min,max,)
+def gen_datePicker(id,min,max,):
     return dcc.DatePickerRange(
         id=id,
         min_date_allowed=min,
         max_date_allowed=max,
         initial_visible_month=dt.today()
     )
-
-# start up app
-app = dash.Dash('test', external_stylesheets=[dbc.themes.SANDSTONE])
-
-app.layout = html.Div(gen_graph(data_set['Enrollments'],'enrollments','enrollments','user_id'))
 
 #if __name__ == "__main__":
 app.run_server(debug=True)
